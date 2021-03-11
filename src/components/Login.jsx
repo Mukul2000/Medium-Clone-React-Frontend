b453844ffe16b9c63fcaac4fb6ad0171ccf60a60
Mukul <mukulkumar627@gmail.com>
2021-03-11 20:25:14 +0530

    API response works and store token in local storage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SignUp from "./SignUp";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 
 export default function Login() {
     const [email, setEmail] = useState("");
-    const [pass, setPassword] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginURL = "https://conduit-medium-clone-api.herokuapp.com/api/users/login";
 
     function validateForm() {
-        return email.length > 0 && pass.length > 0;      
+        return email.length > 0 && password.length > 0;
     }
 
-    function handleSubmit(event) {
-        console.log(email);
-        console.log(pass);
+    async function handleSubmit(event) {
         event.preventDefault();
+        const creds = {
+            "user": {
+                "email": email,
+                "password": password
+            }
+        }
+        
+        axios.post(loginURL, creds)
+        .then((response) => {
+            console.log(response.data);
+            const token = response.data.user.token;
+            localStorage.setItem("token", token);
+        }, (error) => {
+            console.log(error);
+        });
+        
+        
     }
+
 
     return (
         <div className="form-group">
-        <h1> Please log in </h1>
-        <form onSubmit={handleSubmit}> 
-            <label>
-                <p> Email </p>
-                <input 
-                class="form-control"
-                value = {email}
-                autoFocus
-                onChange={(e) => setEmail(e.target.value)} 
-                type="email"/>
-            </label>
-            <label>
-                <p> Password </p>   
-                <input 
-                class="form-control"
-                value = {pass}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"/>
-            </label>
-            <div>
-            <Button type="submit" variant="primary" disabled={!validateForm()}> Submit </Button>
-            </div>
-        </form>
+            <h1> Please log in </h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p> Email </p>
+                    <input
+                        className="form-control"
+                        value={email}
+                        autoFocus
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email" />
+                </label>
+                <div>
+                    <label>
+                        <p> Password </p>
+                        <input
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password" />
+                    </label>
+                </div>
+                <div>
+                    <Button type="submit" className="btn btn-primary" disabled={!validateForm()}> Submit </Button>
+                </div>
+            </form>
         Sign Up
         </div>
     );
