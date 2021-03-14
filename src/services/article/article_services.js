09cae5576fc8cc17fee054fea46fa14b2647b469
Mukul <mukulkumar627@gmail.com>
2021-03-14 22:45:30 +0530

    Finish functionality of create post page minus the successful prompt and redirect

--- src/services/article/article_services.js
+++ src/services/article/article_services.js
@@ -5,16 +5,17 @@ const API_URL = "http://conduit-medium-clone-api.herokuapp.com/api/";
 
 async function createArticle(title, description, body) {
     const article = {
-        headers: authHeader(),
         article: {
             title: title,
             description: description,
             body: body,
         }
     }
-    console.log(article);
-    const response = await axios.post(API_URL + "articles",article);
-    console.log(response);
+    
+    const hedconfig = {
+        headers:authHeader()
+    }
+    const response = await axios.post(API_URL + "articles", article, hedconfig);
     return response.data;
 }
 
