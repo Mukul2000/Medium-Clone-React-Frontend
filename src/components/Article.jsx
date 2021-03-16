9d49b4b2e7527abc34a0d4708dcbaa8c4ee573b7
Mukul <mukulkumar627@gmail.com>
2021-03-16 19:14:05 +0530

    Modify article card

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -9,10 +9,15 @@ export default function Article(props) {
                 articles.map((article, index) => {
                     return (
                         <div class="card text-white bg-dark mb-3">
-                            <div class="card-header"><h2>{article.title}</h2></div>
-                            <div class="card-body">
-                                <h5 class="card-title">{article.description}</h5>
-                                <p class="card-text text-white">{article.body}</p>
+                            <div className="card-header row">
+                                <h2 className="text-left">{article.title}</h2>
+                                <div className="text-right"> 
+                                    {article.author}
+                                </div>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">{article.description}</h5>
+                                <p className="card-text text-white">{article.body}</p>
                             </div>
                         </div>
                     )
